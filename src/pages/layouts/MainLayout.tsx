8f67e3d43798bcdc50e580227ea65a16dfa58075
FelipeLopes1996 <felipel.lima1996@gmail.com>
2025-12-27 11:09:33 -0300

    Create hook use auth guard for redirects

--- src/pages/layouts/MainLayout.tsx
+++ src/pages/layouts/MainLayout.tsx
@@ -1,36 +1,9 @@
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Header from "../../components/Header";
-import { decodeJwt } from "../../utils/decodeJwt";
-import { isTokenExpired } from "../../utils/isTokenExpired";
-import { useEffect } from "react";
-
-const ROUTES_NOT_PROTECT = ["/", "login", "register"];
-
-function isProtectedRoute(pathname: string) {
-  return ROUTES_NOT_PROTECT.includes(pathname);
-}
+import { useAuthGuard } from "../../hooks/useAuthGuard";
 
 export default function MainLayout() {
-  const navigate = useNavigate();
-  const { pathname } = useLocation();
-  const token = decodeJwt(localStorage.getItem("@finance:token") || "");
-
-  useEffect(
-    () => () => {
-      if (
-        token &&
-        isTokenExpired(token?.exp || 0) &&
-        isProtectedRoute(pathname)
-      ) {
-        navigate("/");
-      }
-      if (!isTokenExpired(token?.exp || 0) && !isProtectedRoute(pathname)) {
-        navigate("/dashboard");
-      }
-    },
-    [navigate, pathname, token, token?.exp]
-  );
-
+  useAuthGuard();
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
